64b1c1e750f59a6dac65de3f64b504933ec9bba5
Taddeus Kroes <taddeuskroes@hotmail.com>
2014-01-02 11:24:19 +0100

    Added Github link to copyright notice

--- index.php
+++ index.php
@@ -80,8 +80,11 @@ function getwday($day) {
         </div>
 
         <div class="copyright">
-             &copy; Tadde&uuml;s Kroes 2013 - data wordt opgehaald van
-            <a href="http://www.tvgids.nl/" target="_blank">tvgids.nl</a>
+             &copy; Tadde&uuml;s Kroes 2013
+            - data wordt verzorgd door
+            <a href="http://www.tvgids.nl" target="_blank">tvgids.nl</a>
+            - source code is te vinden op
+            <a href="https://github.com/taddeus/tvgids" target="_blank">Github</a>
         </div>
 
         <script type="text/template" id="details-template">
