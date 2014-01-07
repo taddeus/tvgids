1b801eefa9e92d9f4f3bd41071e9ca95d559bc60
Taddeus Kroes <taddeuskroes@gmail.com>
2014-01-07 19:41:28 +0100

    Adde watch script for sass/coffeescript files

---
+++ watch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+hash inotifywait || (echo "Install inotify-tools first"; exit 1)
+
+make
+
+while true; do
+    inotifywait --quiet --event attrib,modify *.sass *.coffee
+    sleep 0.05s
+    make
+done
