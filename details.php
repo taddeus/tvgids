e34332ec6d3c370b14752153d516b5108a74133a
Taddeus Kroes <taddeuskroes@hotmail.com>
2014-02-10 15:32:30 +0100

    Fixed issue where sometimes the details would be blank

--- details.php
+++ details.php
@@ -6,20 +6,19 @@ $page = file_get_contents($url);
 
 preg_match('/<div\s+id="prog-content">\s*(.*?)\s*<div\s+class="prog-functionbar">/s', $page, $m1);
 assert($m1);
-$description = trim(strip_tags($m1[1], '<p><strong><em><b><i><font><a><span>'));
+$description = strip_tags($m1[1], '<p><strong><em><b><i><font><a><span>');
 $description = str_replace('showVideoPlaybutton()', '', $description);
 $description = preg_replace('/\s+/', ' ', $description);
+$description = htmlentities($description, ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE, 'ISO-8859-1');
+$description = str_replace(array('&lt;', '&gt;'), array('<', '>'), $description);
 
 preg_match('/<ul\s+id="prog-info-content-colleft">\s*(.*?)\s*<\/ul>/s', $page, $m2);
 assert($m2);
 preg_match_all('/<li><strong>(\w+):<\/strong>(.*?)<\/li>/', $m2[1], $m3);
 assert($m3);
-$props = array();
+$properties = array();
 foreach ($m3[1] as $i => $name)
-    $props[] = array('name' => $name, 'value' => $m3[2][$i]);
+    $properties[] = array('name' => $name, 'value' => $m3[2][$i]);
 
-echo json_encode(array(
-    'description' => $description,
-    'properties' => $props
-));
+echo json_encode(compact('description', 'properties'), JSON_UNESCAPED_SLASHES);
 ?>
