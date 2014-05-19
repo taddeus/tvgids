74fab0e6965bcd78df741dd94509532b3cadd9da
Taddeus Kroes <taddeuskroes@hotmail.com>
2014-05-19 14:01:19 +0200

    Moved analytics code to separate file

--- settings.php
+++ settings.php
@@ -47,13 +47,6 @@ function getwday($day) {
         <script src="channels.js" type="text/javascript"></script>
         <script src="settings.js" type="text/javascript"></script>
 
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-13215444-2', 'tkroes.nl');
-            ga('send', 'pageview');
-        </script>
+        <?php include_once 'analytics.php'; ?>
     </body>
 </html>
