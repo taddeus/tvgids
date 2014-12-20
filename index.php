75216cd7c87bb9f4b01d7dc978ac0301e33880bc
Taddeus Kroes <taddeuskroes@hotmail.com>
2014-12-20 21:29:07 +0100

    Removed some unused JS stuff

--- index.php
+++ index.php
@@ -94,13 +94,6 @@ function getwday($day) {
         <script src="lib/jquery-1.10.2.min.js" type="text/javascript"></script>
         <script src="lib/underscore-min.js" type="text/javascript"></script>
         <script src="lib/backbone-min.js" type="text/javascript"></script>
-
-        <!--<script src="lib/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
-        <script src="lib/jquery.kinetic.min.js" type="text/javascript"></script>
-        <script src="lib/jquery.smoothTouchScroll.min.js" type="text/javascript"></script>
-
-        <script src="lib/iscroll-lite.js" type="text/javascript"></script>-->
-
         <script src="channels.js" type="text/javascript"></script>
         <script src="guide.js" type="text/javascript"></script>
 
